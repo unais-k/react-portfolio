7dca14e143cdc8e6408c059d51e900755165d971
unais-k <unaissafa008@gmail.com>
2023-11-26 11:36:33 +0530

    update

--- src/Components/UI/Contact.jsx
+++ src/Components/UI/Contact.jsx
@@ -7,9 +7,17 @@ function Contact() {
                 <h2 className="text-headingColor font-[700] text-[2.5rem] mb-8">Get in touch</h2>
                 <div className="md:flex justify-between items-center">
                     <div className="w-full md:w-1/2 h-[300px] sm:h-[450px]">
-                        <iframe
+                        {/* <iframe
                             title="google-maps"
                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31313.23735662024!2d75.8232162539504!3d11.17618164585027!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba650968482c131%3A0x719dca021ac7bfd7!2sFeroke%2C%20Kerala!5e0!3m2!1sen!2sin!4v1691561655914!5m2!1sen!2sin"
+                            className="border-0 w-full h-full"
+                            allowFullScreen=""
+                            loading="lazy"
+                            referrerPolicy="no-referrer-when-downgrade"
+                        ></iframe> */}
+                        <iframe
+                            title="google-maps"
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d248756.13131189314!2d80.0441988105771!3d13.04747331662676!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a5265ea4f7d3361%3A0x6e61a70b6863d433!2sChennai%2C%20Tamil%20Nadu!5e0!3m2!1sen!2sin!4v1700978719475!5m2!1sen!2sin"
                             className="border-0 w-full h-full"
                             allowFullScreen=""
                             loading="lazy"
